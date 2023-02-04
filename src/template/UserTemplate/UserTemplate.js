edb6d9a5f3fa55f3bfe1bf826256aca25f8ae4d6
anthonynguyen <anthonynguyen1708@gmail.com>
2023-02-04 13:35:32 +0700

    final

--- src/template/UserTemplate/UserTemplate.js
+++ src/template/UserTemplate/UserTemplate.js
@@ -1,11 +1,16 @@
-import { Route } from "react-router-dom";
+import { Redirect, Route } from "react-router-dom";
 import { Fragment, useEffect } from "react";
+import { ENV } from "../../util/settings/config";
 
 export const UserTemplate = (props) => {
   const { Component, ...restProps } = props;
   useEffect(() => {
     window.scrollTo(0, 0);
   });
+
+  if (localStorage.getItem(ENV.USER_PROFILE)) {
+    return <Redirect to="/" />;
+  }
   return (
     <Route
       {...restProps}
